cdf84ebf146d914a8dd69e15e1efa333bd2939da
James Smith <james.smith@djalbat.com>
2021-05-26 16:47:49 +0100

    Corrected the index files.

--- src/browser.js
+++ src/browser.js
@@ -3,5 +3,6 @@
 export { default as ajaxUtilities } from "./utilities/ajax";
 
 export { default as pathUtilities } from "./utilities/path";
+export { default as httpUtilities } from "./utilities/http";
 export { default as arrayUtilities } from "./utilities/array";
 export { default as asynchronousUtilities } from "./utilities/asynchronous";
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 export { default as pathUtilities } from "./utilities/path";
+export { default as httpUtilities } from "./utilities/http";
 export { default as arrayUtilities } from "./utilities/array";
 export { default as asynchronousUtilities } from "./utilities/asynchronous";
 
