60d42f69fc1c4f016f818cfa4df37d2e41c18dbc
James Smith <james.smith@djalbat.com>
2022-01-28 09:28:26 +0000

    Added another content type.

--- src/contentTypes.js
+++ src/contentTypes.js
@@ -1,7 +1,9 @@
 "use strict";
 
 export const APPLICATION_JSON_CONTENT_TYPE = "application/json";
+export const APPLICATION_JSON_CHARSET_UTF_8_CONTENT_TYPE = "application/json; charset=utf-8";
 
 export default {
-  APPLICATION_JSON_CONTENT_TYPE
+  APPLICATION_JSON_CONTENT_TYPE,
+  APPLICATION_JSON_CHARSET_UTF_8_CONTENT_TYPE
 };
