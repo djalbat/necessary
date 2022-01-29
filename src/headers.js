3db5b3dea0fe447d31e33b69c1e4c69cea49bd43
James Smith <james.smith@djalbat.com>
2022-01-29 15:40:15 +0000

    Added another header.

--- src/headers.js
+++ src/headers.js
@@ -2,6 +2,7 @@
 
 export const ACCEPT_HEADER = "accept";
 export const LOCATION_HEADER = "location";
+export const USER_AGENT_HEADER = "user-agent";
 export const CONTENT_TYPE_HEADER = "content-type";
 export const ACCESS_CONTROL_ALLOW_ORIGIN_HEADER = "access-control-allow-origin";
 export const ACCESS_CONTROL_ALLOW_METHODS_HEADER = "access-control-allow-methods";
@@ -11,6 +12,7 @@ export const ACCESS_CONTROL_REQUEST_METHOD_HEADER = "access-control-request-meth
 export default {
   ACCEPT_HEADER,
   LOCATION_HEADER,
+  USER_AGENT_HEADER,
   CONTENT_TYPE_HEADER,
   ACCESS_CONTROL_ALLOW_ORIGIN_HEADER,
   ACCESS_CONTROL_ALLOW_METHODS_HEADER,
