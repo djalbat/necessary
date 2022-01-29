e41662d58f967b2cdeb7e81398084f4d6b6650f2
James Smith <james.smith@djalbat.com>
2022-01-29 17:19:19 +0000

    Added the transfer encoding header.

--- src/headers.js
+++ src/headers.js
@@ -5,6 +5,7 @@ export const LOCATION_HEADER = "location";
 export const USER_AGENT_HEADER = "user-agent";
 export const CONTENT_TYPE_HEADER = "content-type";
 export const CONTENT_LENGTH_HEADER = "content-length";
+export const TRANSFER_ENCODING_HEADER = "transfer-encoding";
 export const ACCESS_CONTROL_ALLOW_ORIGIN_HEADER = "access-control-allow-origin";
 export const ACCESS_CONTROL_ALLOW_METHODS_HEADER = "access-control-allow-methods";
 export const ACCESS_CONTROL_ALLOW_HEADERS_HEADER = "access-control-allow-headers";
@@ -16,6 +17,7 @@ export default {
   USER_AGENT_HEADER,
   CONTENT_TYPE_HEADER,
   CONTENT_LENGTH_HEADER,
+  TRANSFER_ENCODING_HEADER,
   ACCESS_CONTROL_ALLOW_ORIGIN_HEADER,
   ACCESS_CONTROL_ALLOW_METHODS_HEADER,
   ACCESS_CONTROL_ALLOW_HEADERS_HEADER,
