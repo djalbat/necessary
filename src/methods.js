66c762314d5feed9eaddfa2f26a4ab27c4e51d3d
James Smith <james.smith@djalbat.com>
2022-01-29 09:39:49 +0000

    Added to the methods.

--- src/methods.js
+++ src/methods.js
@@ -2,8 +2,10 @@
 
 export const GET_METHOD = "GET";
 export const POST_METHOD = "POST";
+export const OPTIONS_METHOD = "OPTIONS";
 
 export default {
   GET_METHOD,
-  POST_METHOD
+  POST_METHOD,
+  OPTIONS_METHOD
 };
