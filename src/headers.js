e39a4d7aa74ceebb8bf523779eed22c56915511b
James Smith <james.smith@djalbat.com>
2022-01-29 09:37:07 +0000

    Added to the headers.

--- src/headers.js
+++ src/headers.js
@@ -2,8 +2,16 @@
 
 export const ACCEPT_HEADER = "accept";
 export const CONTENT_TYPE_HEADER = "content-type";
+export const ACCESS_CONTROL_ALLOW_ORIGIN_HEADER = "access-control-allow-origin";
+export const ACCESS_CONTROL_ALLOW_METHODS_HEADER = "access-control-allow-methods";
+export const ACCESS_CONTROL_ALLOW_HEADERS_HEADER = "access-control-allow-headers";
+export const ACCESS_CONTROL_REQUEST_METHOD_HEADER = "access-control-request-method";
 
 export default {
   ACCEPT_HEADER,
-  CONTENT_TYPE_HEADER
+  CONTENT_TYPE_HEADER,
+  ACCESS_CONTROL_ALLOW_ORIGIN_HEADER,
+  ACCESS_CONTROL_ALLOW_METHODS_HEADER,
+  ACCESS_CONTROL_ALLOW_HEADERS_HEADER,
+  ACCESS_CONTROL_REQUEST_METHOD_HEADER
 };
