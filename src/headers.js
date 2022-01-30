9d2722a11b64f4a2c75c2536c1de1e35391d3823
James Smith <james.smith@djalbat.com>
2022-01-30 12:09:01 +0000

    Added another header.

--- src/headers.js
+++ src/headers.js
@@ -5,6 +5,7 @@ export const ACCEPT_HEADER = "accept";
 export const LOCATION_HEADER = "location";
 export const USER_AGENT_HEADER = "user-agent";
 export const CONTENT_TYPE_HEADER = "content-type";
+export const AUTHORIZATION_HEADER = "authorization";
 export const CACHE_CONTROL_HEADER = "cache-control";
 export const CONTENT_LENGTH_HEADER = "content-length";
 export const TRANSFER_ENCODING_HEADER = "transfer-encoding";
@@ -20,6 +21,7 @@ export default {
   LOCATION_HEADER,
   USER_AGENT_HEADER,
   CONTENT_TYPE_HEADER,
+  AUTHORIZATION_HEADER,
   CACHE_CONTROL_HEADER,
   CONTENT_LENGTH_HEADER,
   TRANSFER_ENCODING_HEADER,
