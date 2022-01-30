251679b8e9a977fa1ac7f1430ac3a696ef59c8ea
James Smith <james.smith@djalbat.com>
2022-01-30 11:45:41 +0000

    Added the delete method.

--- src/methods.js
+++ src/methods.js
@@ -2,10 +2,12 @@
 
 export const GET_METHOD = "GET";
 export const POST_METHOD = "POST";
+export const DELETE_METHOD = "DELETE";
 export const OPTIONS_METHOD = "OPTIONS";
 
 export default {
   GET_METHOD,
   POST_METHOD,
+  DELETE_METHOD,
   OPTIONS_METHOD
 };
