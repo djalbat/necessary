0ba4e0c93a2f8cfe38a9b683fa753c08d33db856
James Smith <james.smith@djalbat.com>
2023-02-24 18:25:52 +0000

    Put the shift key code back.

--- src/keyCodes.js
+++ src/keyCodes.js
@@ -1,11 +1,13 @@
 "use strict";
 
+export const SHIFT_KEY_CODE = 16;
 export const ENTER_KEY_CODE = 13;
 export const ESCAPE_KEY_CODE = 27;
 export const DELETE_KEY_CODE = 46;
 export const BACKSPACE_KEY_CODE = 8;
 
 export default {
+  SHIFT_KEY_CODE,
   ENTER_KEY_CODE,
   ESCAPE_KEY_CODE,
   DELETE_KEY_CODE,
